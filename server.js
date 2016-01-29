a8d9a5dec29f1d78a197eb71f2269458dfbce51a
Ansuman Mishra <sunmoonfortune@gmail.com>
2016-01-29 11:32:37 +0530

    Adding the morgan and the cors module

--- server.js
+++ server.js
@@ -1,42 +1,102 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var express = require('express'),
+	path = require('path'),
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	mongoose = require('mongoose'),
+	session = require('client-sessions'),
+	cors = require('cors');
 
-// Include the controllers
-var postController = require('./server/controllers/post_controller');
+var routes = require('./routes/index');
+var post = require('./routes/post');
+var comment = require('./routes/comment');
 
-mongoose.connect('mongodb://localhost:27017/blog', function(err) {
-	if(!err) {
-		console.log('Database connection successful');
-	}
-});
+var admin = require('./routes/admin/index');
 
 var app = express();
 
+/* Database connection */
+mongoose.connect('mongodb://localhost/blog');
+var db = mongoose.connection;
+if(db) {
+	console.log('successfully connected to the blog database');
+}
+else {
+	console.log('There is some problem connecting to the blog database');
+}
+/* Database connection */
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/uploads', express.static(__dirname+'/uploads'));
-app.use('/app', express.static(__dirname+'/app'));
+/* Setting session handler */
+app.use(session({
+  cookieName: 'session',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000
+}));
+/* Setting session handler */
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/app/index.html');
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it 
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/post', post); 
+app.use('/post', comment); 
+
+// Admin routes
+app.use('/admin', admin);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/api/post/getAllPosts', postController.getAllPosts);
-app.post('/api/post/create', postController.addEditPosts);
-app.delete('/api/post/:id', postController.deletePost);
-app.get('/api/post/:id', postController.getPostDetail);
+// error handlers
 
-//Getting comments for a post
-app.get('/api/post/:id/comments', postController.getComments);
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-// Adding comment for a post
-app.post('/api/post/:postId/comment/add', postController.addComment);
-
-// Deleting comment for a post
-app.delete('/api/post/:postId/:commentId', postController.deleteComment);
-
-app.listen(3000, function() {
-	console.log('Server is listening in port 3000');
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+
+module.exports = app;
